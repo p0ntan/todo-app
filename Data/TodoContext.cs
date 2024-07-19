3984042853b406f248a023c483aec6d49b4d7174
p0ntan <pontus.akerberg@hotmail.com>
2024-07-19 15:35:53 +0200

    Change name to refreshtokens

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -8,6 +8,7 @@ public class TodoContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<Todo> Todos { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
