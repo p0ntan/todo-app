0981ba3b1ea33fdea7925ae09e05f4a4f238bbdb
p0ntan <pontus.akerberg@hotmail.com>
2024-07-19 16:53:56 +0200

    Add jwt

--- Program.cs
+++ Program.cs
@@ -1,23 +1,36 @@
 using TodoApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TodoContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("TodoContext")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["JWT:Issuer"],  // TODO fix the issuer and audience (and read up)
+            ValidAudience = "TodoApi",
+            ValidateIssuer = false,
+            ValidateAudience = false,  // TODO fix the issuer and audience (and read up)
+            ValidateLifetime = true,   // TODO read up! is lifetime the exp time?
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(builder.Configuration["JWT:AccessTokenSecret"]!)),
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 static void CreateDbIfNotExists(IHost host)
 {
@@ -37,7 +50,17 @@ static void CreateDbIfNotExists(IHost host)
 
 CreateDbIfNotExists(app);
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
