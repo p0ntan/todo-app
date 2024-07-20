8432d87f00ef31588b420e697ef0b1d98324db40
p0ntan <pontus.akerberg@hotmail.com>
2024-07-20 10:08:52 +0200

    Initial commit

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	const paths = [
+		{ name: 'Home', path: '/home' },
+		{ name: 'Calendar', path: '/calendar' },
+		{ name: 'Settings', path: '/settings' }
+	];
+</script>
+
+<nav
+	class="flex w-full border-t border-surface-800 text-surface-300"
+>
+	{#each paths as { name, path }}
+		<button
+			class="btn grow text-lg {$page.url.pathname.includes(path) ? 'text-black' : 'text-surface-500'}"
+			aria-current='page'
+			on:click={() => goto(path)}
+		>
+		<span class="flex flex-col items-center">
+			{name}
+		</span>
+		</button>
+	{/each}
+</nav>
+
+<style>
+	button {
+		border-radius: 0 !important;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.postcss';
+	import Navigation from '$lib/components/Navigation.svelte';
+</script>
+
+<div class="h-screen grid grid-rows-[auto_1fr_auto]">
+	<main class="p-4">
+		<slot />
+	</main>
+	<footer class="fixed bottom-0 w-full"><Navigation /></footer>
+</div>
