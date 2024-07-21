4de94a6f7f35bf9077cc3d836de77d15727f25a0
p0ntan <pontus.akerberg@hotmail.com>
2024-07-21 16:17:40 +0200

    Add spaces

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 export async function handle({ event, resolve }) {
     const token: string | undefined = event.cookies.get('access_token');
-    
+
     let user = null;
 
     if (token) {
@@ -27,7 +27,7 @@ export async function handle({ event, resolve }) {
 export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
 	const refreshToken = event.cookies.get('refresh_token');
 	const accessToken = event.cookies.get('access_token');
-	
+
 	if (request.url.includes("refresh-token") && refreshToken) {
 		request.headers.set('Authorization', `Bearer ${refreshToken}`);
 	} else if (request.url.startsWith(PUBLIC_REST_API_URL) && accessToken) {
