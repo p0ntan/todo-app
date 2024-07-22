474208a92887553d54d8ec8919faf9143c0f9548
p0ntan <pontus.akerberg@hotmail.com>
2024-07-22 12:27:55 +0200

    Add icons and update navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,28 +1,33 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import Home from '$lib/icons/Home.svelte';
+	import Calendar from '$lib/icons/Calendar.svelte';
+	import Customize from '$lib/icons/Customize.svelte';
 
 	const paths = [
-		{ name: 'Home', path: '/home' },
-		{ name: 'Calendar', path: '/calendar' },
-		{ name: 'Settings', path: '/settings' }
+		{ name: 'Home', path: '/home', icon: Home },
+		{ name: 'Calendar', path: '/calendar', icon: Calendar },
+		{ name: 'Settings', path: '/settings', icon: Customize }
 	];
 </script>
 
 <nav
-	class="flex w-full border-t border-surface-800 text-surface-300"
+	class="w-full border-t border-surface-800 bg-white p-4"
 >
-	{#each paths as { name, path }}
-		<button
-			class="btn grow text-lg {$page.url.pathname.includes(path) ? 'text-black' : 'text-surface-500'}"
-			aria-current='page'
-			on:click={() => goto(path)}
-		>
-		<span class="flex flex-col items-center">
-			{name}
-		</span>
-		</button>
-	{/each}
+	<div class="flex bg-gray-200 rounded-full">
+		{#each paths as { name, path, icon }}
+			<button
+				class="btn grow text-lg {$page.url.pathname.includes(path) ? 'fill-black' : 'fill-surface-900'}"
+				aria-current='page'
+				on:click={() => goto(path)}
+			>
+			<span class="flex flex-col items-center max-h-8 max-w-8 p-2 rounded-full">
+				<svelte:component this={icon} />
+			</span>
+			</button>
+		{/each}
+	</div>
 </nav>
 
 <style>
