0d09c75a1d15ea3a98c965b3bf1e1324f2423814
p0ntan <pontus.akerberg@hotmail.com>
2024-07-24 14:13:15 +0200

    Add api-proxy routes

---
+++ src/routes/(auth)/api-proxy/todos/+server.ts
@@ -0,0 +1,28 @@
+import { json } from '@sveltejs/kit';
+import { apiFetch } from '$lib/server/apiFetch.js';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+
+export async function POST({ fetch, cookies, request }) {
+    const data = await request.json();
+
+    try {
+        const response = await apiFetch(fetch, cookies, `${PUBLIC_REST_API_URL}/todos`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            return json(result, { status: response.status });
+        }
+
+        return json(result);
+    } catch (err) {
+        console.error(err);
+        return json({ error: "Unkown server error" }, { status: 500 });
+    }
+}
---
+++ src/routes/(auth)/api-proxy/users/[id]/+server.ts
@@ -0,0 +1,32 @@
+import { json } from '@sveltejs/kit';
+import { apiFetch } from '$lib/server/apiFetch.js';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+import { setAccessCookie } from '$lib/server/setCookies.js';
+
+export async function PUT({ fetch, cookies, request, params }) {
+    const data = await request.json();
+    const id = params.id;
+
+    try {
+        const response = await apiFetch(fetch, cookies, `${PUBLIC_REST_API_URL}/users/${id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            return json(result, { status: response.status });
+        }
+
+        setAccessCookie(cookies, result.data.accessToken);
+
+        return json(result);
+    } catch (err) {
+        console.error(err);
+        return json({ error: "Unkown server error" }, { status: 500 });
+    }
+}
