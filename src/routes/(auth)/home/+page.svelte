54ebfc71e7bb19c533f147c85aa0c007adcbcb60
p0ntan <pontus.akerberg@hotmail.com>
2024-07-24 14:16:07 +0200

    Add form for name and use page for data

--- src/routes/(auth)/home/+page.svelte
+++ src/routes/(auth)/home/+page.svelte
@@ -1,34 +1,58 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import { page } from '$app/stores';
     import { ProgressBar } from '@skeletonlabs/skeleton';
     import Todo from '$lib/components/Todo.svelte';
+    import type { ModalComponent, ModalSettings } from '@skeletonlabs/skeleton';
+    import { getModalStore } from '@skeletonlabs/skeleton';
+    import TodoCreate from '$lib/components/TodoCreate.svelte';
 
-    export let data: PageData;
+    const modalStore = getModalStore();
 
     function updateTodos(todo: any) {
-        const todoIndex = data.todos.findIndex((t: any) => t.id === todo.id);
+        const todoIndex = $page.data.todos.findIndex((t: any) => t.id === todo.id);
 
-        data.todos[todoIndex] = todo;
+        $page.data.todos[todoIndex] = todo;
     }
 
-    $: doneTodos = data.todos.filter((todo: any) => todo.finished);
+    function openCreateModal() {
+        const modalComponent: ModalComponent = {
+            ref: TodoCreate,
+        };
+        const modal: ModalSettings = {
+            type: 'component',
+            component: modalComponent,
+            backdropClasses: '!bg-primary-400 !bg-opacity-80',
+            response: (response: Todo | false) => {
+                if (response) {
+                    console.log("res", response);
+                    
+                    $page.data.todos = [...$page.data.todos, response];
+                }
+            }
+        };
+        modalStore.trigger(modal);
+    }
+
+    $: doneTodos = $page.data.todos.filter((todo: any) => todo.finished);
 </script>
 
 <div class="w-full">
-    <h1 class="h1 w-full mb-4 mt-12">Hello {data?.user?.name}!</h1>
+    <h1 class="h1 w-full mb-4 mt-12">Hello {$page.data.user.name}!</h1>
     
     <div class="card w-full bg-gradient-to-br from-primary-200 to-primary-600 py-6 px-4 shadow-xl mb-4">
         <h3 class="h3 p-0 mb-2">Today's progress</h3>
-        <ProgressBar min={0} max={data.todos.length} value={doneTodos.length} height="h-4" meter="bg-success-300" class="mb-2 shadow-lg"/>
-        <p class="text-white text-end">{doneTodos.length}/{data.todos.length} done</p>
+        <ProgressBar min={0} max={$page.data.todos.length} value={doneTodos.length} height="h-4" meter="bg-success-300" class="mb-2 shadow-lg"/>
+        <p class="text-white text-end">{doneTodos.length}/{$page.data.todos.length} done</p>
     </div>
 
-    <p class="mb-8">
+    <!-- <p class="mb-8">
         A motivation quote could be prestened here, why shoudn’t it?
-    </p>
+    </p> -->
+
+    <button class="btn bg-gradient-to-br from-primary-400 to-primary-600 text-white mx-auto block mb-6" on:click={openCreateModal}>Add todo</button>
     
     <ul class="list w-full px-2">
-        {#each data.todos as todo}
+        {#each $page.data.todos as todo}
             <Todo {todo} on:update={(e) => updateTodos(e.detail)} />
         {/each}
     </ul>
--- src/routes/(auth)/settings/+page.svelte
+++ src/routes/(auth)/settings/+page.svelte
@@ -1,8 +1,45 @@
 <script lang="ts">
+	import { page } from "$app/stores";
+    import { enhance } from "$app/forms";
 
+    const user = $page.data.user
+
+    let firstName = user?.name;
+
+    async function updateUser() {
+        try {
+            const response = await fetch(`${$page.url.origin}/api-proxy/users/${user.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ firstName })
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to update user.');
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
 </script>
 
-<div class="w-full">
-    <h1 class="h1 w-full mb-4 mt-12">Settings</h1>
+<div class="w-full h-full flex flex-col justify-between">
+    <div>
+        <h1 class="h1 w-full mb-4 mt-12">Settings</h1>
+    
+        <div>
+            <label class="label">
+                <span>Name:</span>
+                <input type="text" name="firstname" class="input rounded-md" bind:value={firstName}>
+            </label>
 
+            <button class="btn bg-gradient-to-br from-success-200 to-success-300 mx-auto block mb-6" on:click={updateUser}>Save</button>
+        </div>
+    </div>
+
+    <form method="post" action="/settings?/logout" class="justify-end" use:enhance>
+        <button class="btn bg-gradient-to-br from-error-400 to-error-500 text-white mx-auto block mb-6">Log out</button>
+    </form>
 </div>
