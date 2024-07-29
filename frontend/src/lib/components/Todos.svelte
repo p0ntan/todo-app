816f45d623d5a34511d02651b4a864982c16d034
p0ntan <pontus.akerberg@hotmail.com>
2024-07-29 13:21:34 +0200

    Add simple filtering of todos

--- frontend/src/lib/components/Todos.svelte
+++ frontend/src/lib/components/Todos.svelte
@@ -3,8 +3,11 @@
     import TodoComponent from '$lib/components/Todo.svelte';
     import { createEventDispatcher } from 'svelte';
 
-    const dispatch = createEventDispatcher();
     export let date = new Date().toISOString().slice(0, 10);
+    
+    const dispatch = createEventDispatcher();
+    let filterDone: boolean = false;
+    let filterLeft: boolean = false;
 
     function updateTodos() {
         todos = todos.filter((t: Todo) => t.date.includes(date));
@@ -19,10 +22,44 @@
 	}
 
     export let todos: Todo[];
+
+    $: doneTodos = todos.filter((t: Todo) => t.finished);
+    $: leftTodos = todos.filter((t: Todo) => !t.finished);
+    $: filteredTodos = filterDone ? filterLeft ? leftTodos : doneTodos : filterLeft ? leftTodos : todos;
 </script>
 
+<div class="flex bg-gray-200 rounded-full mb-6">
+        <button on:click={() => {filterDone = false; filterLeft = true}}
+            class="btn grow {filterLeft
+                ? 'text-black'
+                : 'text-gray-500'}"
+        >
+            <span class="flex flex-col items-center rounded-full">
+                Left
+            </span>
+        </button>
+        <button on:click={() => {filterDone = true; filterLeft = false}}
+            class="btn grow {filterDone
+                ? 'text-black'
+                : 'text-gray-500'}"
+        >
+            <span class="flex flex-col items-center rounded-full">
+                Done
+            </span>
+        </button>
+        <button on:click={() => {filterDone = false; filterLeft = false}}
+            class="btn grow {(!filterDone && !filterLeft)
+                ? 'text-black'
+                : 'text-gray-500'}"
+        >
+            <span class="flex flex-col items-center rounded-full">
+                All
+            </span>
+        </button>
+</div>
+
 <ul class="list w-full px-2">
-    {#each todos as todo}
+    {#each filteredTodos as todo}
         <TodoComponent {todo} on:update={updateTodos} on:delete={(e) => deleteTodo(e.detail)} />
     {/each}
 </ul>
